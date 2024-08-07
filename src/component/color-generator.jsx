c500df70bf7805fe95907b054037494acec57757
al-aminh <alaminhossain104023@gmail.com>
2024-08-07 17:22:02 +0600

    second commit

--- src/component/color-generator.jsx
+++ src/component/color-generator.jsx
@@ -1,9 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Generator = () => {
+    const [typeofColor, setTypeofColor] = useState('hex');
+    const [color, setColor] = useState('#ffffff');
+    const randomNumber = (length) => {
+        return Math.floor(Math.random()*length)
+    }
+    const generateRandomHexColor = () => {
+        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+        let hexColor = '#';
+        for(let i = 0; i < 6; i++){
+            hexColor += hex[randomNumber(hex.length)]
+        }
+        setColor(hexColor);
+        console.log(hexColor);
+    }
+    const generateRandomRgbColor = () => {
+        const rgb = `rgb(${randomNumber(256)},${randomNumber(256)},${randomNumber(256)})`;
+        setColor(rgb);
+        console.log(rgb);
+
+    }
     return (
         <>
-        <div>Hello world!</div>
+        <div style={{backgroundColor:color}} className="h-screen w-screen">
+            <div>
+                <button
+                    onClick={() => setTypeofColor('rgb')}
+                >RGB</button>
+                <button 
+                    onClick={() => setTypeofColor('hex')}
+                >HEX</button>
+                <button 
+                    onClick={typeofColor === 'hex'? generateRandomHexColor:generateRandomRgbColor}
+                >Generate Random {typeofColor} color</button>
+            </div>
+        </div>
         </>
     )
 }
