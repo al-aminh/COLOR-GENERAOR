abb19b86572c4144f9cde3f2bc5b7c0e40304d2a
al-aminh <alaminhossain104023@gmail.com>
2024-08-07 18:33:09 +0600

    style added

--- src/component/color-generator.jsx
+++ src/component/color-generator.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 
 const Generator = () => {
     const [typeofColor, setTypeofColor] = useState('hex');
-    const [color, setColor] = useState('#ffffff');
+    const [color, setColor] = useState('#c9daBf');
     const randomNumber = (length) => {
         return Math.floor(Math.random()*length)
     }
@@ -23,17 +23,30 @@ const Generator = () => {
     }
     return (
         <>
-        <div style={{backgroundColor:color}} className="h-screen w-screen">
-            <div>
+        <div style={{backgroundColor:color}} className="h-screen w-screen flex justify-center">
+            <div className="flex flex-col m-6 text-white items-center gap-5 p-3">
+                <div className="flex gap-5">
                 <button
+                    className="bg-purple-400 p-2 rounded-xl text-center font-sans font-bold shadow-md"
                     onClick={() => setTypeofColor('rgb')}
                 >RGB</button>
                 <button 
+                    className="bg-purple-400 p-2 rounded-xl text-center font-sans font-bold shadow-md"
                     onClick={() => setTypeofColor('hex')}
                 >HEX</button>
                 <button 
+                    className="bg-purple-400 p-2 rounded-xl text-center font-sans font-bold shadow-md"
                     onClick={typeofColor === 'hex'? generateRandomHexColor:generateRandomRgbColor}
                 >Generate Random {typeofColor} color</button>
+                </div>
+                <div className="flex flex-col gap-5 text-cente">
+                <h3 
+                    className="bg-purple-400 p-2 rounded-xl text-center font-sans font-bold shadow-md"
+                >Color Type: {typeofColor}</h3>
+                <h3 
+                    className="bg-purple-400 p-2 rounded-xl text-center font-sans font-bold shadow-md"
+                >Color Code: {color}</h3>
+                </div>
             </div>
         </div>
         </>
